b3c63e0c29ec1081fef4d77b68ae2139e23a847d
suchhanda046 <suchhanda.senapati@citrusad.com>
2023-09-04 12:44:48 +0530

    Changes done for PR testing

---
+++ src/test/java/swaglabs/sampletest/TestClass.java
@@ -0,0 +1,21 @@
+package swaglabs.sampletest;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import swaglabs.BaseClass;
+
+ class TestClass extends BaseClass{
+	@Test
+	public void testMethod() {	
+		driver.get("https://wwww.facebook.com");
+		System.out.print("Test PR");	
+		}
+	@Test
+	public void testMethodPR() {	
+		System.out.print("Test PR....");	
+		System.out.print("Checkout to branch - Test PR done");
+		}
+}
